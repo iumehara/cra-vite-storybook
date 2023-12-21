8677aa781201181fe45aaf147b04d568d1186f6b
Ichizo Umehara <iumehara@vmware.com>
2023-12-21 10:05:43 -0500

    Extract parts into components

---
+++ src/Main/Spinner/Spinner.tsx
@@ -0,0 +1,7 @@
+import logo from '../../logo.svg';
+import React from 'react';
+import './Spinner.scss';
+
+export const Spinner = () => {
+  return <img src={logo} className="Spinner" alt="logo"/>;
+};
